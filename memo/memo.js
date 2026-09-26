19df30337f585437907ed0a33ee260d41a7ce88a
bisqhq <bisqhq@gmail.com>
2026-09-26 09:52:45 +0900

    Update memo.js

--- memo/memo.js
+++ memo/memo.js
@@ -25,7 +25,7 @@ const memoPosts = [
         id: "post-20260926",
         date: "2026.09.26",
         text: "あまりにもブルスカにいるので追記にリンクしておきます。",
-        moreText: "",
+        moreText: "<a href=\"https://bsky.app/profile/sankaq.bsky.social\" target=\"_blank\">こちら</a>9割自我ですので悪しからず。",
     },
     {
         id: "post-20260922",
